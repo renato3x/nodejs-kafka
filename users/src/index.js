73e3d2df7ce09d2ed83144dc7c8e579b7f007fbb
renato3x <pereirarenato21@gmail.com>
2023-03-31 09:39:49 -0300

    refatoração

--- users/src/index.js
+++ users/src/index.js
@@ -4,12 +4,14 @@ require('./database/connection')
 const { router } = require('./routes')
 const { ErrorHandler } = require('./middlewares/ErrorHandler')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(ErrorHandler)
+app.use(cors())
 app.use('/v1', router)
 
 module.exports = { app }
